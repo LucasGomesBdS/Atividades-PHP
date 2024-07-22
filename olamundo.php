b8e1ed67d3f36d9e16d8bdf691bd9bf201ce154a
Lucas Gomes <163914471+LucasGomesBdS@users.noreply.github.com>
2024-07-22 18:16:27 -0300

    Add files via upload

---
+++ olamundo.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1>Minha primeira página HTML</h1>
+    <?php 
+    echo "ola mundo!";
+    ?>
+
+
+</body>
+</html>
